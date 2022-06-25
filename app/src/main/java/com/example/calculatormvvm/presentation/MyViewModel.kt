234827de63065bf4e6a44308d32cacaf176c786f
Bozhenko Roman <35889412+BozhenkoR@users.noreply.github.com>
2022-06-25 21:06:37 +0300

    coroutine commit

--- app/src/main/java/com/example/calculatormvvm/presentation/MyViewModel.kt
+++ app/src/main/java/com/example/calculatormvvm/presentation/MyViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.calculatormvvm.domain.Calculator
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 
 class MyViewModel() : ViewModel() {
@@ -28,38 +30,55 @@ class MyViewModel() : ViewModel() {
     private val resultOfDropEndSymbolMutable = MutableLiveData<String>()
     val resultOfDropEndSymbol: LiveData<String> = resultOfDropEndSymbolMutable
 
-    var job: Job? = null
+    private val coroutineScope = CoroutineScope(Job() + Dispatchers.Default)
+//    private lateinit var job:Job
+
+
 
 
     override fun onCleared() {
         super.onCleared()
         Log.d(TAG, "MyViewModel_onCleared")
 
+        coroutineScope.cancel()
+        Log.d(TAG, "MyViewModel_job_cancel")
+
 
     }
 
-    fun calculate(stringForCalc: String)/* = CoroutineScope(Dispatchers.Default).launch()*/ {
+    fun calculate(stringForCalc: String) /* = CoroutineScope(Dispatchers.Default).launch */ {
         Log.d(TAG, "MyViewModel.calculate")
+        /**
+        * CoroutineContext - Dispatchers, id, name, Exception
+        **/
 
-        job = CoroutineScope(Dispatchers.Default).launch {
+
+//        job.launch{}
+
+//        job = CoroutineScope(Dispatchers.Default).launch {
+
+//        job = CoroutineScope(SupervisorJob() + Dispatchers.Default).launch {}
+
+//        job = CoroutineScope(SupervisorJob()).launch {
+
+        coroutineScope.launch {
 
 //            resultOfCalcMutable.postValue(calculator.calculate(stringForCalc))
-//            job?.cancel()
 
             val result = calculator.calculate(stringForCalc)
-
+            Log.d(TAG, "Thread_Name >>> ${Thread.currentThread().name}" + " || result = $result")
 
             withContext(Dispatchers.Main) {
                 if (result.isNotEmpty()) {
                     resultOfCalcMutable.value = result
+                    Log.d(TAG, "Thread_Name >>> ${Thread.currentThread().name}")
                 } else {
                     Log.e(TAG, "MyViewModel.calculate.Error")
                 }
-                job?.cancel()
             }
-
         }
     }
+
 //    fun calculate(stringForCalc: String) {
 //        Log.d(TAG, "MyViewModel.calculate")
 //        resultOfCalcMutable.value = calculator.calculate(stringForCalc)
